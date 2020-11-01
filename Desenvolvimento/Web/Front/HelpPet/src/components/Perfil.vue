015b05e2afb754f1d32205496a7446e651f9ec1a
sadinho <50503203+sadinho@users.noreply.github.com>
2020-11-01 20:08:51 -0300

    desenvolvimento perfil

--- Desenvolvimento/Web/Front/HelpPet/src/components/Perfil.vue
+++ Desenvolvimento/Web/Front/HelpPet/src/components/Perfil.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="entrar w3-col">
-    <center class="w3-blue" style="height: 30vh;">
-        <span><i class="fas fa-user-cog"></i></span>
-        <span class="circulo w3-white w3-margin-top">
-            <img class="perfil" src="../assets/imagens/logo.png">
-        </span>
-    </center>
+    <span><i class="fas fa-user-cog w3-display-topleft w3-text-white w3-margin-left w3-margin-top"></i></span>
+    <span><i class="fas fa-sign-out-alt w3-display-topright w3-text-white w3-margin-right w3-margin-top"></i></span>
+    <div class="w3-blue w3-mobile w3-center" style="height: 30vh; margin-top:0px!important;">
+        <span class="w3-text-white w3-center fonte-titulo">Perfil</span>
+        <div class="w3-center w3-col s6 m6 l12">
+            <span class="circulo w3-white">
+                <img class="perfil w3-center" src="../assets/imagens/avatar.jpg">
+            </span>
+        </div>
+    </div>
+    
+    <div class="w3-center" style="margin-top: 50px;">
+         <!-- Interruptor arredondado (toggle) -->
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider round w3-text-gray bold">
+                <span class="w3-col m6 s6 l6 font-verde" style="margin-left:2%!important;">Help! Pets</span>
+                <span class="w3-col m6 s6 l6" style="margin-left:50%!important;">Meus Pets</span>
+            </span>
+        </label>
+    </div>
+   
     
   </div>
 </template>
@@ -23,8 +39,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-img{
-  width: 10%;
-  height: 20%;
-}
+
 </style>
