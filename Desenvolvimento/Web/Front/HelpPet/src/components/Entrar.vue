b0f4d6ea88cd557db420f8f7967fb78b3338bb76
sadinho <50503203+sadinho@users.noreply.github.com>
2020-11-01 13:27:14 -0300

    ajuste

---
+++ Desenvolvimento/Web/Front/HelpPet/src/components/Entrar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="entrar">
+    <img src="../assets/imagens/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Entrar',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img{
+  width: 10vw;
+  height: 20vh;
+}
+</style>
