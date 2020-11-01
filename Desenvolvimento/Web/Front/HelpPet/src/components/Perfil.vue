3af139675462bcc179e3244e28b90c531db20a4b
sadinho <50503203+sadinho@users.noreply.github.com>
2020-11-01 17:04:22 -0300

    desenvolvimento

--- Desenvolvimento/Web/Front/HelpPet/src/components/Perfil.vue
+++ Desenvolvimento/Web/Front/HelpPet/src/components/Perfil.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="entrar w3-center w3-col">
-    <center class="w3-blue">
-        <span>Conficurações</span>
-        <div class="circulo w3-red">
-            <img class="w3-center" src="../assets/imagens/logo.png">
-            <i class="fas fa-trash"></i>
-        </div>
+  <div class="entrar w3-col">
+    <center class="w3-blue" style="height: 30vh;">
+        <span><i class="fas fa-user-cog"></i></span>
+        <span class="circulo w3-white w3-margin-top">
+            <img class="perfil" src="../assets/imagens/logo.png">
+        </span>
     </center>
     
   </div>
