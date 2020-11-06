8ca54b77ea9c787c489399105e211a5031cd20e8
sadinho <50503203+sadinho@users.noreply.github.com>
2020-11-06 17:09:25 -0300

    desenvolvimento

--- Desenvolvimento/FrontEnd/src/components/Entrar.vue
+++ Desenvolvimento/FrontEnd/src/components/Entrar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="entrar w3-center w3-col">
     <!--<span class="w3-text-green w3-margin-top w3-col s6 m6 l6"><h1>Entrar</h1></span>-->
-    <img class="w3-col s6 m6 l6" src="../assets/imagens/logo.png">
+    <img class="centralizado" src="../assets/imagens/logo.png">
 
     <form class="w3-container">
       <input class="w3-input w3-margin-bottom" type="text" placeholder="Email">
       <input class="w3-input w3-margin-bottom w3-margin-top" type="text"  placeholder="Senha">
-      <button class="w3-btn w3-purple w3-round-xxlarge w3-margin-top  w3-margin-bottom w3-col">Entrar</button>
+      <button class="w3-btn w3-blue w3-round-xxlarge w3-margin-top  w3-margin-bottom w3-col">Entrar</button>
       <span class="w3-text-green w3-margin-top">Perdeu sua senha?</span>
     </form>
   </div>
@@ -26,7 +26,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 img{
-  width: 10%;
-  height: 20%;
+  max-width: 25vw;
+  height: 35vw;
 }
 </style>
