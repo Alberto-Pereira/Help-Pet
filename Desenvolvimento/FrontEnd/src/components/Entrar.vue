bbb6948eeab712c46ca8e19ea4bfcf2e90750fe2
sadinho <50503203+sadinho@users.noreply.github.com>
2020-11-06 20:37:11 -0300

    desenvolvimento componente de mensagem

--- Desenvolvimento/FrontEnd/src/components/Entrar.vue
+++ Desenvolvimento/FrontEnd/src/components/Entrar.vue
@@ -1,23 +1,57 @@
 <template>
+  
   <div class="entrar w3-center w3-col">
     <!--<span class="w3-text-green w3-margin-top w3-col s6 m6 l6"><h1>Entrar</h1></span>-->
     <img class="centralizado" src="../assets/imagens/logo.png">
 
     <form class="w3-container">
-      <input class="w3-input w3-margin-bottom" type="text" placeholder="Email">
-      <input class="w3-input w3-margin-bottom w3-margin-top" type="text"  placeholder="Senha">
-      <button class="w3-btn w3-blue w3-round-xxlarge w3-margin-top  w3-margin-bottom w3-col">Entrar</button>
-      <span class="w3-text-green w3-margin-top">Perdeu sua senha?</span>
+      <input class="w3-input w3-margin-bottom" type="text" v-model="email" placeholder="Email">
+      <input class="w3-input w3-margin-bottom w3-margin-top" type="text" v-model="senha" placeholder="Senha">
+      <button @click="logar()" class="w3-btn w3-blue w3-round-xxlarge w3-margin-top  w3-margin-bottom w3-col">Entrar</button>
+      <span @click="recuperarSenha()" class="w3-text-green w3-margin-top">Perdeu sua senha?</span>
     </form>
+    
+    <mensagem ref="enviaMensagem"/>
   </div>
 </template>
 
 <script>
+import Mensagem from '@/components/Mensagem'
 export default {
+  components:{
+    Mensagem
+  },
   name: 'Entrar',
   data () {
     return {
-      
+      senha:'',
+      email:''
+    }
+  },
+  
+  methods:{
+    logar(){
+      let usuario = this.email.substring(0, this.email.indexOf("@"));
+      let dominio = this.email.substring(this.email.indexOf("@")+ 1, this.email.length);
+      if ((usuario.length >=1) &&
+          (dominio.length >=3) &&
+          (usuario.search("@")==-1) &&
+          (dominio.search("@")==-1) &&
+          (usuario.search(" ")==-1) &&
+          (dominio.search(" ")==-1) &&
+          (dominio.search(".")!=-1) &&
+          (dominio.indexOf(".") >=1)&&
+          (dominio.lastIndexOf(".") < dominio.length - 1))
+        {
+          this.$refs.enviaMensagem.exclamar("sucesso", "E-mail válido");
+        }
+        else{
+          this.$refs.enviaMensagem.exclamar("erro", "E-mail inválido");
+        }
+
+    },
+    recuperarSenha(){
+
     }
   }
 }
---
+++ Desenvolvimento/FrontEnd/src/components/Mensagem.vue
@@ -0,0 +1,71 @@
+<template>
+    <div v-if="ativado" class="w3-modal w3-animate-zoom" style="display: block; background-color:transparent;">
+        <div class="w3-modal-content w3-border w3-round-xlarge " :class="getEstilo()">
+            <div class="w3-container">
+                <i :class="getIcone()"></i> <strong>{{mensagem}}</strong>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "Mensagem",
+        data() {
+            return {
+            tipo: '',
+            mensagem: '',
+            ativado: false,
+            };
+        },
+        methods: {
+            exclamar(tipo, mensagem){
+                this.tipo = tipo;
+                this.mensagem = mensagem;
+                this.ativado = true
+                setTimeout(this.ativado = true, 90000000000);
+                
+            },
+            getEstilo(){
+                if(this.tipo == "info"){
+                    return "w3-yellow";
+                }
+                if(this.tipo == "erro"){
+                    return "w3-red";
+                }
+                if(this.tipo == "sucesso"){
+                    return "w3-green";
+                }
+            },
+            getIcone(){
+                if(this.tipo == "info"){
+                    return "fas fa-info-circle";
+                }
+                if(this.tipo == "erro"){
+                    return "fas fa-exclamation-triangle";
+                }
+                if(this.tipo == "sucesso"){
+                    return "far fa-thumbs-up";
+                }
+            }
+            
+        }
+    }
+</script>
+
+<style scoped>
+.detalhe-foto-pet{
+  max-width: 90%;
+  border-radius: 25px;
+  width: 200px;
+  height: 200px;
+}
+#fileInput {
+  display: none;
+}
+.my-8 {
+  margin-top: 0rem;
+  margin-bottom: 0rem;
+}
+</style>
