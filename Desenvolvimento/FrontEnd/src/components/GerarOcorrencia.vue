a3acdbe5547200a6fb36c981ea5ab4cf8815bc99
Douglas de Freitas <freitas.douglas3@gmail.com>
2020-11-17 22:00:07 -0300

    Foi criado uma ação para o botão (Buscar nova foto do pet), foi removido o botão filtrar pois não vi uso para o mesmo.

--- Desenvolvimento/FrontEnd/src/components/GerarOcorrencia.vue
+++ Desenvolvimento/FrontEnd/src/components/GerarOcorrencia.vue
@@ -9,16 +9,11 @@
         </router-link>
       </h6>
       <h3 class="w3-col s8 w3-center font-verde bold-500">Gerar ocorrência</h3>
-      <h6 class="w3-col s2 padding-10 font-amarela">
-        <router-link to="">
-          <i class="fas fa-filter"></i>
-        </router-link>
-      </h6>
     </div>
 
     <div class="padding-10">
       <div class="w3-center">
-        <img class="border-img" src="../assets/imagens/cachorrinho.png" alt="Girl in a jacket" width="80" height="80">
+        <img class="border-img" :src="image" alt="" width="80" height="80">
       </div>
       <input class="w3-input w3-margin-top w3-text-black bold-500" type="text" placeholder="Nome do pet: (preenchido)">
       <input class="w3-input w3-margin-top w3-text-black bold-500" type="text" placeholder="Raça: (preenchido)">
@@ -28,8 +23,23 @@
     </div>
 
     <div class="margin-top-30">
-      <button class="w3-col w3-btn w3-round-xxlarge w3-center fundo-azul-claro w3-text-white w3-margin-top">Buscar nova foto do pet</button>
-      <button class="w3-col w3-btn w3-round-xxlarge w3-center fundo-roxo w3-text-white w3-margin-top">Gravar dados</button>
+      <label for="fileInput" slot="upload-label">
+        <span class="w3-col w3-btn w3-round-xxlarge w3-center fundo-azul-claro w3-text-white w3-margin-top">Buscar nova foto do pet</span>
+      </label>
+      <image-uploader style="display: none"
+        class="w3-col"
+        :quality="1.00"
+        :preview="false"
+        :className="['fileinput', { 'fileinput--loaded': hasImage }]"
+        capture="user"
+        :debug="1"
+        doNotResize="gif"
+        :autoRotate="true"
+        outputFormat="string"
+        @input="setImage"
+      ></image-uploader>
+
+      <button @click="gravarDados" class="w3-col w3-btn w3-round-xxlarge w3-center fundo-roxo w3-text-white w3-margin-top">Gravar dados</button>
     </div>
 
   </div>
@@ -37,8 +47,34 @@
 </template>
 
 <script>
+
+  import ImageUploader from 'vue-image-upload-resize'
     export default {
-        name: "GerarOcorrencia"
+        name: "GerarOcorrencia",
+        components: {
+          ImageUploader,
+        },
+        data : ()=> {
+          return {
+            hasImage : false,
+            image : require('../assets/imagens/cachorrinho.png')
+          }
+        },
+        mounted () {
+
+        },
+        methods: {
+          prencherTela : function (){
+            //TODO implementar
+          },
+          setImage: function (file) {
+            this.hasImage = true;
+            this.image = file;
+          },
+          gravarDados : function () {
+            //TODO implementar
+          }
+        }
     }
 </script>
 
