4ff36006d5304eccf75332cc6a254df3f9d6b46a
Pedronex <pedrosoares.nex@gmail.com>
2020-12-04 18:40:14 -0300

    Mudando cors

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
-app.use(cors());
 app.use(express.json());
 app.use(routes);
 
