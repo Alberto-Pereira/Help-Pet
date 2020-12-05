1d0e4999039ec9d14411f8c2cd33f5990ad70f85
Pedronex <pedrosoares.nex@gmail.com>
2020-12-05 17:21:55 -0300

    arrumando o login

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,17 +3,18 @@ const crypto = require("crypto");
 
 module.exports = {
   async show(req, res) {
-    console.log("Entrou");
     const { email, password } = req.body;
-    const crypt = crypto.createHash("sha1").update(password).digest("hex");
+    const crypt = crypto
+      .createHash("sha1")
+      .update(password)
+      .digest("hex");
     const user = await connection("usuario")
       .where("senha", crypt)
       .where("email", email);
     if (user.length == 0) {
       res.status(401).send("Email ou senha incorreto");
     } else {
-      console.log("Não entrou");
-      res.status(200).send("Logado com sucesso");
+      res.status(200).json(user[0]);
     }
   },
 };
