a2e544c22757027d426bbbd59f1f1c94cc7ebb7a
Pedronex <pedrosoares.nex@gmail.com>
2020-12-05 15:55:20 -0300

    Teste

--- src/server.js
+++ src/server.js
@@ -4,8 +4,7 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(cors());
-app.options("*", cors());
+app.use(cors("45.226.43.154", "201.34.242.70", "177.207.229.187"));
 
 app.use(express.json());
 app.use(routes);
