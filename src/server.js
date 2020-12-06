16539644d326ea1a4cc5939397cefacbd09860f8
Pedronex <pedrosoares.nex@gmail.com>
2020-12-05 23:06:25 -0300

    otimização de codigo

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const routes = require("./routes");
 const app = express();
 app.use(cors("*", "201.34.242.70", "177.207.229.187", "45.226.43.154"));
 app.use(express.json({ limit: "50mb" }));
-app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
